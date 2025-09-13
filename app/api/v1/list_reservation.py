496a851d31037d509af1a3694e3714e69af63d16
Guilherme <guicaruaru@hotmail.com>
2025-09-13 12:59:00 -0300

    modificando a varivael `property_email` para `client_email`

--- app/api/v1/list_reservation.py
+++ app/api/v1/list_reservation.py
@@ -7,15 +7,15 @@ router = APIRouter()
 
 @router.get("/list_reservations/", status_code = 200)
 def list_reservations(property_id: int = Query(None, description="Informe o ID da propriedade para listar as reservas"),
-                      property_email: str = Query(None, description="Informe o email do cliente para listar as reservas"),
+                      client_email: str = Query(None, description="Informe o email do cliente para listar as reservas"),
                       db: Session = Depends(get_db)):
     
     if property_id:
         reservations_list = db.query(Reservation).filter(Reservation.property_id == property_id).all()
         return reservations_list
 
-    if property_email:
-        reservations_list = db.query(Reservation).filter(Reservation.client_email == property_email).all()
+    if client_email:
+        reservations_list = db.query(Reservation).filter(Reservation.client_email == client_email).all()
         return reservations_list
     
     
