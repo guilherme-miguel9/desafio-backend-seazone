7165ac8cdfdbf86d7a7678d36bad588a9103063a
Guilherme <guicaruaru@hotmail.com>
2025-09-13 12:53:25 -0300

    criação da lista de reservas, ele pode filtrar pelo ID da propriedade quanto pelo e-mail do cliente, `list_reservation.py`

---
+++ app/api/v1/list_reservation.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter, Depends, HTTPException, Query
+from sqlalchemy.orm import Session
+from app.db.database import get_db
+from app.db.models import Reservation
+
+router = APIRouter()
+
+@router.get("/list_reservations/", status_code = 200)
+def list_reservations(property_id: int = Query(None, description="Informe o ID da propriedade para listar as reservas"),
+                      property_email: str = Query(None, description="Informe o email do cliente para listar as reservas"),
+                      db: Session = Depends(get_db)):
+    
+    if property_id:
+        reservations_list = db.query(Reservation).filter(Reservation.property_id == property_id).all()
+        return reservations_list
+
+    if property_email:
+        reservations_list = db.query(Reservation).filter(Reservation.client_email == property_email).all()
+        return reservations_list
+    
+    
