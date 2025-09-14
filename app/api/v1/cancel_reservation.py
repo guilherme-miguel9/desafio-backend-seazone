286e82f78e447d23e3a21446bb10060b1290aa10
Guilherme <guicaruaru@hotmail.com>
2025-09-14 16:52:25 -0300

    endpoint para cancelar uma reserva, method= DELETE, status_code= 204, cancela pelo ID da Reserva

---
+++ app/api/v1/cancel_reservation.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter, Depends, HTTPException, Query
+from sqlalchemy.orm import Session
+from app.db.database import get_db
+from app.db.models import Reservation
+
+router = APIRouter()
+
+@router.delete("/cancel_reservation/", status_code=204)
+def cancel_reservation(reservation_id: int = Query(..., description="ID da reserva para ser cancelada"),
+                       db: Session = Depends(get_db)):
+    
+    reservation = db.query(Reservation).filter(Reservation.id == reservation_id).first()
+
+    if not reservation:
+        raise HTTPException(status_code=404, detail='Reserva nao foi encontrada')
+    
+    db.delete(reservation)
+    db.commit()
+    
