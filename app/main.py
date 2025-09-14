0869af0958eb4dfc1aabfc540f17f0ea305fc488
Guilherme <guicaruaru@hotmail.com>
2025-09-14 16:52:51 -0300

    atualizando as rotas do arquivo main.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.api.v1 import properties, list_properties, reservation, list_reservation
+from app.api.v1 import properties, list_properties, reservation, list_reservation, cancel_reservation, availability
 
 app = FastAPI()
 
@@ -7,3 +7,5 @@ app.include_router(properties.router, prefix="/api/v1")
 app.include_router(list_properties.router, prefix="/api/v1")
 app.include_router(reservation.router, prefix="/api/v1")
 app.include_router(list_reservation.router, prefix="/api/v1")
+app.include_router(cancel_reservation.router, prefix="/api/v1")
+app.include_router(availability.router, prefix="/api/v1")
